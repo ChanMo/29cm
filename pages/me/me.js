d6716cbc2fbd2f2491ab24e3f284bf35aff5d672
MacBook <chen@MacBook-Air.local>
2018-12-05 17:49:36 +0800

    add me

---
+++ pages/me/me.js
@@ -0,0 +1,18 @@
+const app = getApp()
+
+Page({
+  data: {
+    link: []
+  },
+  onLoad: function() {
+    this.fetchLink()
+  },
+
+  /**
+   * 获取链接
+   */
+  fetchLink: function() {
+    let url = app.globalData.domain + 'link/USER_LINK/'
+    wx.request({url, success:res=>this.setData({link:res.data})})
+  }
+})
