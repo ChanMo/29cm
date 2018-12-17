0fc74117c5902e427001924e267f0414e961d614
MacBook <chen@MacBook-Air.local>
2018-12-17 15:07:26 +0800

    use api

--- pages/order/order.js
+++ pages/order/order.js
@@ -2,23 +2,23 @@ const app = getApp()
 
 Page({
   data: {
-    data: []
+    id: 0,
+    data: {}
   },
-
-  onLoad: function() {
+  onLoad: function(options) {
+    this.setData({id: options.id})
     this.fetchData()
   },
-
   /**
    * 获取订单数据
    */
   fetchData: function() {
-    let url = app.globalData.domain + 'order'
+    let url = app.globalData.domain + 'order/' + this.data.id + '/'
     wx.request({
-      url:url,
+      url: url,
       header: {
         'Authorization': 'Token ' + wx.getStorageSync('token')
       },
-      success:res=>this.setData({data: res.data.results})})
+      success:res=>this.setData({data: res.data})})
   }
 })
