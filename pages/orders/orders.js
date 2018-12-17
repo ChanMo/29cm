505cf55456dd7722fbe27f6609387ba6278710d6
MacBook <chen@MacBook-Air.local>
2018-12-17 14:57:38 +0800

    add orders

---
+++ pages/orders/orders.js
@@ -0,0 +1,32 @@
+const app = getApp()
+
+Page({
+  data: {
+    data: []
+  },
+
+  onLoad: function() {
+    this.fetchData()
+  },
+
+  /**
+   * 获取订单数据
+   */
+  fetchData: function() {
+    let url = app.globalData.domain + 'order'
+    wx.request({
+      url:url,
+      header: {
+        'Authorization': 'Token ' + wx.getStorageSync('token')
+      },
+      success:res=>this.setData({data: res.data.results})})
+  },
+
+  /**
+   * 订单详情
+   */
+  onGoDetail: function(e) {
+    let id = e.currentTarget.dataset.id
+    wx.navigateTo({url: '/pages/order/order?id=' + id})
+  }
+})
